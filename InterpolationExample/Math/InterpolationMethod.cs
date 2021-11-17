0f842645e92532e588574b5be89ea8d02ca3e5f3
Ermek <fascwind@gmail.com>
2021-11-17 11:03:09 +0500

    fix interpolation error

--- InterpolationExample/Math/InterpolationMethod.cs
+++ InterpolationExample/Math/InterpolationMethod.cs
@@ -10,7 +10,7 @@ namespace InterpolationExample
     {
         public static double InterpolateDouble(Point point1, Point point2, double x)
         {
-            return point1.Y + (x - point1.X) * (point2.Y - point1.Y)/(point2.X - point1.X);
+            return point1.Y + x * (point2.Y - point1.Y)/(point2.X - point1.X);
         }
     }
 }
